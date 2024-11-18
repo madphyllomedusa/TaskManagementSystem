e6998459428c15729d96b80ed2d1b3ecf1944962
Phyllomedusa <matthew.vladimirovich@gmail.com>
2024-11-18 15:48:36 +0300

    Нужно обработку админа перенести в securityFilterChain, а так вроде все есть.

--- src/main/java/ru/test/taskmanagementsystem/model/entity/Task.java
+++ src/main/java/ru/test/taskmanagementsystem/model/entity/Task.java
@@ -1,13 +1,24 @@
 package ru.test.taskmanagementsystem.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.test.taskmanagementsystem.model.enums.Priority;
 import ru.test.taskmanagementsystem.model.enums.Status;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 
 @Getter
