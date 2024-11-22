380ac9269b500ee93b718af5e931763572a84085
Phyllomedusa <matthew.vladimirovich@gmail.com>
2024-11-22 06:08:36 +0300

    check check

--- src/test/java/ru/test/taskmanagementsystem/controller/TaskControllerTest.java
+++ src/test/java/ru/test/taskmanagementsystem/controller/TaskControllerTest.java
@@ -29,7 +29,10 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
